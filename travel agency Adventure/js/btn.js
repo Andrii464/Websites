664340bbccac506a74723d459bb36bb7076b8ac8
Andrii464 <58467480+Andrii464@users.noreply.github.com>
2021-05-16 18:00:53 +0300

    add

--- travel agency Adventure/js/btn.js
+++ travel agency Adventure/js/btn.js
@@ -1,9 +1,22 @@
-let light = document.querySelector("#theme");
-const change = (theme, prevtheme, button, btnclick) => {
-    let btn = document.querySelector(button);
-    let btnprev = document.querySelector(btnclick);
-    btn.style.display = 'block';
-    light.classList.add(theme);
-    light.classList.remove(prevtheme);
-    btnprev.style.display = 'none';
+let light = document.getElementById("onLight");
+let dark = document.getElementById("onDark");
+let hd = document.getElementById("hd");
+let intr = document.getElementById("intr");
+
+
+const change = (light_theme) => {
+    if (light_theme) {
+        hd.style.background  ="rgba(0,0,0,0.4)";
+        dark.style.display = 'block';
+        light.style.display = 'none';
+		intr.style.background = "url('img/andr.jpg') center no-repeat";
+		intr.style.backgroundSize = "cover";
+    } else {
+        hd.style.background = "rgba(255,255,255,0.4)"
+        dark.style.display = 'none';
+        light.style.display = 'block';
+		intr.style.background = "url('img/intro.jpg') center no-repeat";
+		intr.style.backgroundSize = "cover";
+		
+    }
 }
